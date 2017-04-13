5275da5d462b7e76eec87413374a3ba59a9d08f7
Marijn Otte <marijnotte94@gmail.com>
2017-04-13 10:54:54 +0200

    features start

--- Dissimilarity.m
+++ Dissimilarity.m
@@ -1,4 +1,4 @@
-m = prnist(0:9,1:1:1000);
+m = prnist(0:9,1:125:1000);
 
 data = seldat(m);
 %resizing images so every image is same size
@@ -7,13 +7,113 @@ dataset = prdataset(resized);
 %%
 [trainset,testset] = gendat(dataset,.5);
 repset = gendat(trainset,0.25); 
-disspace = proxm(repset,'d',1);
+disspace = proxm(repset,'c',1);
 trainrep = trainset*disspace; 
 %%
-classf = trainrep*rnnc;
+
+
+classf = trainrep*fisherc;
 e = testset*disspace*classf*testc;   
 %scatterd(trainset);               
 num2str(e,'%5.3f')
+
+classf = trainrep*nmc;
+e = testset*disspace*classf*testc;   
+%scatterd(trainset);               
+num2str(e,'%5.3f')
+
+classf = trainrep*qdc;
+e = testset*disspace*classf*testc;   
+%scatterd(trainset);               
+num2str(e,'%5.3f')
+
+classf = trainrep*ldc;
+e = testset*disspace*classf*testc;   
+%scatterd(trainset);               
+num2str(e,'%5.3f')
+
+classf = trainrep*loglc;
+e = testset*disspace*classf*testc;   
+%scatterd(trainset);               
+num2str(e,'%5.3f')
+
+classf = trainrep*knnc;
+e = testset*disspace*classf*testc;   
+%scatterd(trainset);               
+num2str(e,'%5.3f')
+
+classf = trainrep*parzenc;
+e = testset*disspace*classf*testc;   
+%scatterd(trainset);               
+num2str(e,'%5.3f')
+
+classf = trainrep*treec;
+e = testset*disspace*classf*testc;   
+%scatterd(trainset);               
+num2str(e,'%5.3f')
+
+classf = trainrep*neurc;
+e = testset*disspace*classf*testc;   
+%scatterd(trainset);               
+num2str(e,'%5.3f')
+
+classf = trainrep*svc;
+e = testset*disspace*classf*testc;   
+%scatterd(trainset);               
+num2str(e,'%5.3f')
+
+%%
+%{
+classf = trainrep*[fisherc,nmc];
+e = testset*disspace*classf*testc;   
+%scatterd(trainset);               
+num2str(e,'%5.3f')
+
+classf = trainrep*[fisherc,ldc];
+e = testset*disspace*classf*testc;   
+%scatterd(trainset);               
+num2str(e,'%5.3f')
+
+classf = trainrep*[fisherc,knnc];
+e = testset*disspace*classf*testc;   
+%scatterd(trainset);               
+num2str(e,'%5.3f')
+
+classf = trainrep*[fisherc,parzenc];
+e = testset*disspace*classf*testc;   
+%scatterd(trainset);               
+num2str(e,'%5.3f')
+
+classf = trainrep*[nmc, ldc];
+e = testset*disspace*classf*testc;   
+%scatterd(trainset);               
+num2str(e,'%5.3f')
+
+classf = trainrep*[nmc, knnc];
+e = testset*disspace*classf*testc;   
+%scatterd(trainset);               
+num2str(e,'%5.3f')
+
+classf = trainrep*[nmc, parzenc];
+e = testset*disspace*classf*testc;   
+%scatterd(trainset);               
+num2str(e,'%5.3f')
+
+classf = trainrep*[ldc, knnc];
+e = testset*disspace*classf*testc;   
+%scatterd(trainset);               
+num2str(e,'%5.3f')
+
+classf = trainrep*[ldc, parzenc];
+e = testset*disspace*classf*testc;   
+%scatterd(trainset);               
+num2str(e,'%5.3f')
+
+classf = trainrep*[knnc, parzenc];
+e = testset*disspace*classf*testc;   
+%scatterd(trainset);               
+num2str(e,'%5.3f')
+%}
 
 
     
--- Feature.m
+++ Feature.m
@@ -1,20 +1,40 @@
-m = prnist(0:9,1:5:1000);
+m = prnist(0:9,1:200:1000);
+
+%%
+data = seldat(m);
+resized = im_resize(data,[128,128],'bicubic');
+
+%datafeatures = im_features(resized);
+profile = im_features(resized);
+dataset_profile = prdataset(profile);
+%datamoments = im_moments(resized);
+plote(clevalf(dataset_profile,pcam))
+%%
+%dataset = [datafeatures prdataset(dataprofile) prdataset(datamoments)];
+clf = {nmc,ldc,knnc,parzenc,fisherc,loglc,neurc,treec}*pcam;
+mf = 23;
+featnum = [1:1:23]
+datascaled = dataset_profile*scalem(dataset_profile,'variance');
+[Test,Train]=gendat(datascaled,.5);
+[w,r] = featselb(Train, 'eucl-m',mf)
+e1 = clevalf(Train*w,clf,featnum,[],1,Test*w);
+figure;
+plote(e1,'noapperror')
+%%
+m = prnist(0:9,1:400:1000);
 
 data = seldat(m);
-%resizing images so every image is same size
 resized = im_resize(data,[128,128],'bicubic');
-datafeatures = im_features(resized);
-dataprofile = im_profile(resized);
-datamoments = im_moments(resized);
 
-dataset = [datafeatures prdataset(dataprofile) prdataset(datamoments)];
+features = im_profile(resized);
+A = prdataset(profile);
+W = A * pcam;
+B = A*W;
+V = B*featsel;
+E = clevalf(A*W,{nmc,ldc,knnc,parzenc,fisherc,loglc,neurc,treec});
+figure;
+plote(E,'noapperror');
+axis([1 23 0 1])
 
-datascaled = dataset*scalem(dataset,'variance');
 
-datapca = datascaled*pcam(datascaled,20);
-
-W = {ldc,knnc([],5)};
-
-[Train,Test]=gendat(datapca(:,:),.7);
-E = testc(Test,Train*W)
-
+%}
