f836836e381b862d1eabffa4a2ca2e5c8551638e
Jamey Sparreboom <jamey@computerspar.nl>
2017-04-20 09:43:47 +0200

    added pixel representation

--- Pixel_representation.m
+++ Pixel_representation.m
@@ -1,11 +1,21 @@
-m = prnist(0:9,1:5:1000);
+m = prnist(0:9,1:1:1000);
 
 data = seldat(m);
 %resizing images so every image is same size
-resized = im_resize(data,[5,5],'bicubic');
+data = im_resize(data,[15,15],'bicubic');
+data = im_norm(data);
+dataset = prdataset(data);
 
-dataset = prdataset(resized);
-W = {parzenc([],0.5),knnc([],6)};
+%%
+[trainset,testset] = gendat(dataset,0.5);
+repset = trainset; %gendat(trainset, [5,5,5,5,5,5,5,5,5,5]);
 
-[Train,Test]=gendat(dataset(:,:),.7);
-E = testc(Test,Train*W)
+%
+%classifiers = append([], knnc(repset,2), knnc(repset,3), knnc(repset,4), nmc(repset), ldc(repset), loglc(repset), parzenc(repset, 0.25), parzenc(repset, 0.5), parzenc(repset, 0.75), treec(repset), neurc(repset));
+classf = knnc(repset, 3);
+e = testset*classf*testc;   
+%scatterd(trainset);
+disp(num2str(e,'%5.3f'))
+disp('Calculating using nist_eval')
+e = nist_eval('my_rep',classf);
+disp(num2str(e,'%5.3f'))
